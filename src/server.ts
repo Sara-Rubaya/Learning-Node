970630a4aae73b35438bdb48b188586a3f11ffae
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-11 15:11:37 +0600

    got a better file organization

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,9 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+import { routeHandler } from "./routes/route";
 
 
 const server : Server = createServer((req : IncomingMessage, res: ServerResponse) =>{
-    // console.log(req.url); =>'/', '/user', '/product'....
-    // console.log(req.method); => "GET", "POST", "Delete"...
-
-    const url = req.url
-    const method = req.method
-
-    if(url === '/' && method === "GET"){
-        // console.log("This is Root route");
-        res.writeHead(200,{"content-type" : "application/json"})
-        res.end(JSON.stringify({message: "This is Root route"}));
-    } else if(url?.startsWith("/products")){
-        res.writeHead(200,{"content-type" : "application/json"})
-        res.end(JSON.stringify({message: "This is product route"}));
-    }else{
-         res.writeHead(404,{"content-type" : "application/json"})
-        res.end(JSON.stringify({message: "Route not found!"}));
-    }
+   routeHandler(req,res);
  },
 );
 
